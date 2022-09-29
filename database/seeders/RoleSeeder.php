ff96e99c1b8302895f5d6646c40c7b08a75d1be3
JorgeJmnz <jorgejmnz08@gmail.com>
2022-09-29 16:12:02 -0500

    a

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+$role1 = Role::create(['name'->'Admin']);
+$role2 = Role::create(['name'->'Reader']);
+$role3 = Role::create(['name'->'Writer']);
+
+Permission::create(['name' -> 'leer'])
+    }
+}
